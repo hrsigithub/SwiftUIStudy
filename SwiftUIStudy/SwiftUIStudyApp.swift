7366079f40e3eadb30ff6184368868e20232c36d
Hiroshi.Nakai <hrsi.ad@gmail.com>
2020-10-12 14:58:43 +0900

    Initial Commit

---
+++ SwiftUIStudy/SwiftUIStudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIStudyApp.swift
+//  SwiftUIStudy
+//
+//  Created by Hiroshi.Nakai on 2020/10/12.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIStudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
