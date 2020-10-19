f0279ac5dd69bb82dcb045136ba094efb859c2cb
Hiroshi.Nakai <hrsi.ad@gmail.com>
2020-10-19 13:08:44 +0900

    Apple 公式 Study 1 追加

---
+++ SwiftUIStudy/TextView.swift
@@ -0,0 +1,20 @@
+//
+//  TextView.swift
+//  SwiftUIStudy
+//
+//  Created by Hiroshi.Nakai on 2020/10/13.
+//
+
+import SwiftUI
+
+struct TextView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TextView_Previews: PreviewProvider {
+    static var previews: some View {
+        TextView()
+    }
+}
